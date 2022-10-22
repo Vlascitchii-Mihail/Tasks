26d348da2b4a1c528828f6a20beefad5ab732ee2
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-10-22 11:28:12 +0300

    Baseball

--- Android/AndroidBaseBallLeague/app/src/main/java/dev/mfazio/androidbaseballleague/util/BindingAdapters.kt
+++ Android/AndroidBaseBallLeague/app/src/main/java/dev/mfazio/androidbaseballleague/util/BindingAdapters.kt
@@ -1,6 +1,8 @@
 package dev.mfazio.androidbaseballleague.util
 
+import android.view.View
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 
 /**
@@ -9,4 +11,11 @@ import androidx.databinding.BindingAdapter
 @BindingAdapter("android:src")
 fun setImageViewResource(imageView: ImageView, resourceId: Int) {
     imageView.setImageResource(resourceId)
+}
+
+@BindingAdapter("android:backgroundTint")
+fun setBackgroundTint(view: View, colorId: Int) {
+    view.background.setTintList(
+        ContextCompat.getColorStateList(view.context, colorId)
+    )
 }

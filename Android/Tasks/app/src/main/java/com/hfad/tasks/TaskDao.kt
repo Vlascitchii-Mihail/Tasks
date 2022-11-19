8e0ad184949b33080fe1c0f18e4cae283e14ceb8
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-11-19 08:47:56 +0200

    Task

---
+++ Android/Tasks/app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -0,0 +1,23 @@
+package com.hfad.tasks
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Insert
+    fun insert(task: Task)
+
+    @Update
+    fun update(task: Task)
+
+    @Delete
+    fun delete(task: Task)
+
+    @Query("SELECT * FROM task_table WHERE taskId = :taskId")
+    fun get(taskId: Long): LiveData<Task>
+}
